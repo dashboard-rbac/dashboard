f66b76969dd8e93684a7c2b51ea25a02f57a4a6d
Bruno Ricardo Corrêa <60991787+brunorcorrea@users.noreply.github.com>
2024-04-30 20:04:43 -0300

    small fixes

--- graphs/posts-by-knowledge-area.js
+++ graphs/posts-by-knowledge-area.js
@@ -1,31 +1,34 @@
+var labels = [
+  "Artes visuais e performáticas",
+  "Conhecimentos gerais",
+  "Lingua portuguesa",
+  "Tecnologia e Inovação",
+  "História",
+  "Ciências",
+  "Geografia",
+  "Matemática",
+  "Física",
+  "Sociologia",
+  "Biologia",
+  "Filosofia",
+  "Química",
+  "Educação física",
+  "Ensino religioso",
+];
+
+var data = [
+  300, 300, 170, 100, 350, 350, 300, 580, 580, 250, 80, 595, 580, 570, 270,
+];
+
 var ctx = document.getElementById("posts-by-knowledge-area");
 var chartGraph = new Chart(ctx, {
   type: "bar",
   data: {
-    labels: [
-      "Artes visuais e performáticas",
-      "Conhecimentos gerais",
-      "Lingua portuguesa",
-      "Tecnologia e Inovação",
-      "História",
-      "Ciências",
-      "Geografia",
-      "Matemática",
-      "Física",
-      "Sociologia",
-      "Biologia",
-      "Filosofia",
-      "Química",
-      "Educação física",
-      "Ensino religioso",
-    ],
+    labels: labels,
     datasets: [
       {
         label: "Quantidade de postagens por área de conhecimento",
-        data: [
-          300, 300, 170, 100, 350, 350, 300, 580, 580, 250, 80, 595, 580, 570,
-          270,
-        ],
+        data: data,
         borderWidth: 1,
         borderColor: "rgba(17,146,232,1)",
         backgroundColor: "rgba(17,146,232,0.9)",
