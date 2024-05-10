f5058d8223d97995fa8378592eb5ec7dbcb0e68f
Mitsuo Miyazato <mitsuoluann@hotmail.com>
2024-05-10 15:04:56 -0300

    fix requests

--- graphs/map.js
+++ graphs/map.js
@@ -121,15 +121,6 @@ function onEachFeature(feature, layer) {
   });
 }
 
-function replaceSpecialChars(str) {
-  str = str.replace(/[ÀÁÂÃÄÅ]/, "A");
-  str = str.replace(/[ÈÉÊË]/, "E");
-  str = str.replace(/[Ç]/, "C");
-  str = str.replace(/[ç]/, "c");
-
-  return str.replace(/[^a-z0-9]/gi, "");
-}
-
 var maxValue = -1;
 
 (async () => {
