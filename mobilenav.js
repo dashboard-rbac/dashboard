499fc4f30e738bdd63a687133bc21ff0685ee220
niiandreatti <nicolasandreatti5@gmail.com>
2024-05-15 21:55:55 -0300

    responsive

--- mobilenav.js
+++ mobilenav.js
@@ -1,41 +1,53 @@
 class MobileNavBar {
-    constructor(mobileMenu, navList, navLinks){
-        this.mobileMenu = document.querySelector(mobileMenu);
-        this.navList = document.querySelector(navList);
-        this.navLinks = document.querySelectorAll(navLinks);
-        this.activeClass = "active";
-        this.handleClick = this.handleClick.bind(this);
-    }
+  constructor(mobileMenu, navList, navLinks, submenus) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.submenus = document.querySelectorAll(submenus);
+    this.activeClass = "active";
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    animateLinks(){
-        this.navLinks.forEach((link) => {
-            link.style.animation
-            ? (link.style.animation = "")
-            : (link.style.animation = `navLinkFade 0.5s ease forwards 0.2s`)
-        });
-    }
-    
-    handleClick(){
-        this.navList.classList.toggle(this.activeClass);
-        this.mobileMenu.classList.toggle(this.activeClass);
-        this.animateLinks();
-    }
+  animateLinks() {
+    this.navLinks.forEach((link) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards 0.2s`);
+    });
+  }
 
-    addClickEvent(){
-        this.mobileMenu.addEventListener("click", this.handleClick);
-    }
+  adjustSubmenuPosition() {
+    this.submenus.forEach((submenu) => {
+      if (submenu.classList.contains(this.activeClass)) {
+        const menuRect = submenu.parentElement.getBoundingClientRect();
+        submenu.style.top = `${menuRect.top}px`;
+      }
+    });
+  }
 
-    init(){
-        if(this.mobileMenu){
-            this.addClickEvent();
-        }
-        return this;
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+    this.adjustSubmenuPosition(); 
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
     }
+    return this;
+  }
 }
 
 const mobileNavBar = new MobileNavBar(
-    ".mobile-menu",
-    "#menu",
-    "#menu li",
+  ".mobile-menu",
+  "#menu",
+  "#menu li",
+  ".submenu"
 );
 mobileNavBar.init();
